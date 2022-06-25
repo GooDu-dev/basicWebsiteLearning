c4f78d2723a1bb32caf5932333d6602a5db8082b
GooDu-Dev <mokumokutv@gmail.com>
2022-06-25 20:12:45 +0700

    week5

---
+++ ForLearning/week5/week5/app.js
@@ -0,0 +1,10 @@
+function buttonAlert(){
+    console.log("alert")
+    window.alert("Clicked")
+}
+
+function changeColor(){
+    let head = document.getElementById("head")
+    head.style.color = "white"
+    head.style.backgroundColor = "black"
+}
